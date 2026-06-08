e1653ef9e48c2f92b5cd653263557c34b81feb46
Anil Jaiswal <63344805+anil402jaiswal@users.noreply.github.com>
2026-06-08 15:54:07 +0530

    Update addition.py

--- GitHub-Actions-Zero-to-Hero-main/src/addition.py
+++ GitHub-Actions-Zero-to-Hero-main/src/addition.py
@@ -1,8 +1,6 @@
-# app.py
-# This is a test commit by mr. anil jaiswal
 def add(a, b):
     return a + b
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+if __name__ == "__main__":
+    result = add(5, 10)
+    print(f"The sum of 5 and 10 is: {result}")
